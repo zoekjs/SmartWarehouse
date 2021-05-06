94b174e0ec19a49f5092e20d89c59549ce929474
zoekjs <zoek.esbu@gmail.com>
2021-05-06 18:59:22 -0400

    Revert "Feature/cambio clave (#28)"
    This reverts commit 564dfa398a9a0df3c2767e9bd34f0db02be5f2cb.

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\User;
 use Illuminate\Foundation\Auth\ResetsPasswords;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class ResetPasswordController extends Controller
 {
@@ -37,27 +34,6 @@ class ResetPasswordController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except(['index', 'update']);
-    }
-
-    public function index()
-    {
-        return view('auth.passwords.change');
-    }
-
-    public function update(Request $request, $rut_user)
-    {
-        $request->validate([
-            'password' => 'required|string|min:6|confirmed'
-        ]);
-
-        $user = User::find($rut_user);
-
-        if (!empty($request->password) && !empty($request->password_confirmation))
-        {
-            $user->password = Hash::make($request->password);
-            $user->save();
-            return back()->with('success', 'Contraseña cambiada exitosamente');
-        }
+        $this->middleware('guest');
     }
 }
